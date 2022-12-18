ffc2bc682148c01a6b04e99cc4e034f15a8c495f
unknown <happyjaskass@mail.ru>
2022-12-18 18:38:15 +0500

    DopEx done

---
+++ DopEx/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Clear();
+Console.WriteLine("Write a sequence of numbers : ");
+int number = Convert.ToInt32(Console.ReadLine());
+int max = 0;
+int max2 = 0;
+while (number != 0)
+{
+    number = Convert.ToInt32(Console.ReadLine());
+    if (max < number)
+    {
+        max2 = max;
+        max = number;
+    }
+    else if (max != number && max2 < number)
+    {
+        max2 = number;
+    }
+}
+Console.WriteLine(max2);
