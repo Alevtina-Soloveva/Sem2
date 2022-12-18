4b40c7fecc48d836804cfeeaf0df316f9ab5acee
unknown <happyjaskass@mail.ru>
2022-12-18 17:31:42 +0500

    Ex13 done

---
+++ Ex13/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+Console.WriteLine("Write your number : ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number / 100 == 0)
+    Console.Write("there is no third number");
+else
+{
+    while (Math.Abs(number) >= 1000)
+    {
+    number = number / 10;
+    }
+    Console.WriteLine(Math.Abs(number) % 10);
+}
