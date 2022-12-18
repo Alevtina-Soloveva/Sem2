1673512d7aa180903e42bd4563e4a767ac93ad6b
unknown <happyjaskass@mail.ru>
2022-12-18 17:46:49 +0500

    Ex15 done

---
+++ Ex15/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Clear();
+Console.WriteLine("Write your number : ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number == 6 || number == 7)
+    Console.Write("yes");
+else 
+    Console.Write("no");
